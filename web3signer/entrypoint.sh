43da986e9ed58f3d1550597724d21ba80baa3adb
Diego <dsimog01@estudiantes.unileon.es>
2023-07-19 13:37:01 +0200

    Update Dockerfiles

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
 export KEYFILES_DIR="/data/keyfiles"
-export NETWORK="prater"
+export NETWORK="lukso"
 export WEB3SIGNER_API="http://web3signer.web3signer-${NETWORK}.dappnode:9000"
 
-# Assign proper value to _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER. The UI uses the web3signer domain in the Header "Host"
-case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER" in
-"prysm-prater.dnp.dappnode.eth")
-  ETH2_CLIENT_DNS="validator.prysm-prater.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.prysm-prater.dappnode:3500"
-  export CLIENT_API="http://validator.prysm-prater.dappnode:3500"
+# Assign proper value to LUKSO_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO. The UI uses the web3signer domain in the Header "Host"
+case "$LUKSO_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO" in
+"prysm-${NETWORK}.dnp.dappnode.eth")
+  ETH2_CLIENT_DNS="validator.prysm-${NETWORK}.dappnode"
+  export BEACON_NODE_API="http://beacon-chain.prysm-${NETWORK}.dappnode:3500"
+  export CLIENT_API="http://validator.prysm-${NETWORK}.dappnode:3500"
   export TOKEN_FILE="/security/prysm/auth-token"
   export CLIENTS_TO_REMOVE=(teku lighthouse lodestar nimbus)
   ;;
-"teku-prater.dnp.dappnode.eth")
-  ETH2_CLIENT_DNS="validator.teku-prater.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.teku-prater.dappnode:3500"
-  export CLIENT_API="https://validator.teku-prater.dappnode:3500"
+"teku-${NETWORK}.dnp.dappnode.eth")
+  ETH2_CLIENT_DNS="validator.teku-${NETWORK}.dappnode"
+  export BEACON_NODE_API="http://beacon-chain.teku-${NETWORK}.dappnode:3500"
+  export CLIENT_API="https://validator.teku-${NETWORK}.dappnode:3500"
   export TOKEN_FILE="/security/teku/validator-api-bearer"
   export CLIENTS_TO_REMOVE=(prysm lighthouse lodestar nimbus)
   ;;
-"lighthouse-prater.dnp.dappnode.eth")
-  ETH2_CLIENT_DNS="validator.lighthouse-prater.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.lighthouse-prater.dappnode:3500"
-  export CLIENT_API="http://validator.lighthouse-prater.dappnode:3500"
+"lighthouse-${NETWORK}.dnp.dappnode.eth")
+  ETH2_CLIENT_DNS="validator.lighthouse-${NETWORK}.dappnode"
+  export BEACON_NODE_API="http://beacon-chain.lighthouse-${NETWORK}.dappnode:3500"
+  export CLIENT_API="http://validator.lighthouse-${NETWORK}.dappnode:3500"
   export TOKEN_FILE="/security/lighthouse/api-token.txt"
   export CLIENTS_TO_REMOVE=(teku prysm lodestar nimbus)
   ;;
-"nimbus-prater.dnp.dappnode.eth")
-  ETH2_CLIENT_DNS="beacon-validator.nimbus-prater.dappnode"
-  export BEACON_NODE_API="http://beacon-validator.nimbus-prater.dappnode:4500"
-  export CLIENT_API="http://beacon-validator.nimbus-prater.dappnode:3500"
+"nimbus-${NETWORK}.dnp.dappnode.eth")
+  ETH2_CLIENT_DNS="beacon-validator.nimbus-${NETWORK}.dappnode"
+  export BEACON_NODE_API="http://beacon-validator.nimbus-${NETWORK}.dappnode:4500"
+  export CLIENT_API="http://beacon-validator.nimbus-${NETWORK}.dappnode:3500"
   export TOKEN_FILE="/security/nimbus/auth-token"
   export CLIENTS_TO_REMOVE=(teku lighthouse lodestar prysm)
   ;;
-"lodestar-prater.dnp.dappnode.eth")
-  ETH2_CLIENT_DNS="validator.lodestar-prater.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.lodestar-prater.dappnode:3500"
-  export CLIENT_API="http://validator.lodestar-prater.dappnode:3500"
+"lodestar-${NETWORK}.dnp.dappnode.eth")
+  ETH2_CLIENT_DNS="validator.lodestar-${NETWORK}.dappnode"
+  export BEACON_NODE_API="http://beacon-chain.lodestar-${NETWORK}.dappnode:3500"
+  export CLIENT_API="http://validator.lodestar-${NETWORK}.dappnode:3500"
   export TOKEN_FILE="/security/lodestar/api-token.txt"
   export CLIENTS_TO_REMOVE=(teku lighthouse prsym nimbus)
   ;;
 *)
-  echo "_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_PRATER env is not set properly"
+  echo "LUKSO_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO env is not set properly"
   exit 1
   ;;
 esac
@@ -55,12 +55,11 @@ if grep -Fq "/opt/web3signer/keyfiles" ${KEYFILES_DIR}/*.yaml ;then
 fi
 
 # Run web3signer binary
-# - Run key manager (it may change in the future): --key-manager-api-enabled=true
 exec /opt/web3signer/bin/web3signer \
   --key-store-path="$KEYFILES_DIR" \
   --http-listen-port=9000 \
   --http-listen-host=0.0.0.0 \
-  --http-host-allowlist="web3signer.web3signer-prater.dappnode,brain.web3signer-prater.dappnode,$ETH2_CLIENT_DNS" \
+  --http-host-allowlist="web3signer.web3signer-${NETWORK}.dappnode,brain.web3signer-${NETWORK}.dappnode,$ETH2_CLIENT_DNS" \
   --http-cors-origins=* \
   --metrics-enabled=true \
   --metrics-host 0.0.0.0 \
@@ -68,8 +67,8 @@ exec /opt/web3signer/bin/web3signer \
   --metrics-host-allowlist="*" \
   --idle-connection-timeout-seconds=900 \
   eth2 \
-  --network=prater \
-  --slashing-protection-db-url=jdbc:postgresql://postgres.web3signer-prater.dappnode:5432/web3signer \
+  --network=${CHAIN_CONFIG_FILE_PATH} \
+  --slashing-protection-db-url=jdbc:postgresql://postgres.web3signer-${NETWORK}.dappnode:5432/web3signer \
   --slashing-protection-db-username=postgres \
   --slashing-protection-db-password=password \
   --slashing-protection-pruning-enabled=true \
