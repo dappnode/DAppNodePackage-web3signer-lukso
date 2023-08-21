25a491ff89fc8ef78ddfa0d23e68b52afab0d4b2
Diego <dsimog01@estudiantes.unileon.es>
2023-08-21 18:33:01 +0200

    Remove unnecesary envs

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -8,49 +8,29 @@ export WEB3SIGNER_API="http://web3signer.web3signer-${NETWORK}.dappnode:9000"
 case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO" in
 "prysm-${NETWORK}.dnp.dappnode.eth")
   ETH2_CLIENT_DNS="validator.prysm-${NETWORK}.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.prysm-${NETWORK}.dappnode:3500"
-  export CLIENT_API="http://validator.prysm-${NETWORK}.dappnode:3500"
-  export TOKEN_FILE="/security/prysm/auth-token"
-  export CLIENTS_TO_REMOVE=(teku lighthouse lodestar nimbus)
   ;;
 "teku-${NETWORK}.dnp.dappnode.eth")
   ETH2_CLIENT_DNS="validator.teku-${NETWORK}.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.teku-${NETWORK}.dappnode:3500"
-  export CLIENT_API="https://validator.teku-${NETWORK}.dappnode:3500"
-  export TOKEN_FILE="/security/teku/validator-api-bearer"
-  export CLIENTS_TO_REMOVE=(prysm lighthouse lodestar nimbus)
   ;;
 "lighthouse-${NETWORK}.dnp.dappnode.eth")
   ETH2_CLIENT_DNS="validator.lighthouse-${NETWORK}.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.lighthouse-${NETWORK}.dappnode:3500"
-  export CLIENT_API="http://validator.lighthouse-${NETWORK}.dappnode:3500"
-  export TOKEN_FILE="/security/lighthouse/api-token.txt"
-  export CLIENTS_TO_REMOVE=(teku prysm lodestar nimbus)
   ;;
 "nimbus-${NETWORK}.dnp.dappnode.eth")
   ETH2_CLIENT_DNS="beacon-validator.nimbus-${NETWORK}.dappnode"
-  export BEACON_NODE_API="http://beacon-validator.nimbus-${NETWORK}.dappnode:4500"
-  export CLIENT_API="http://beacon-validator.nimbus-${NETWORK}.dappnode:3500"
-  export TOKEN_FILE="/security/nimbus/auth-token"
-  export CLIENTS_TO_REMOVE=(teku lighthouse lodestar prysm)
   ;;
 "lodestar-${NETWORK}.dnp.dappnode.eth")
   ETH2_CLIENT_DNS="validator.lodestar-${NETWORK}.dappnode"
-  export BEACON_NODE_API="http://beacon-chain.lodestar-${NETWORK}.dappnode:3500"
-  export CLIENT_API="http://validator.lodestar-${NETWORK}.dappnode:3500"
-  export TOKEN_FILE="/security/lodestar/api-token.txt"
-  export CLIENTS_TO_REMOVE=(teku lighthouse prsym nimbus)
   ;;
 *)
-  echo "_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO env is not set properly"
-  exit 1
+  echo "_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO env is unknown. Setting ${_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO} as ETH2_CLIENT_DNS"
+  ETH2_CLIENT_DNS=$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_LUKSO
   ;;
 esac
 
 # IMPORTANT! The dir defined for --key-store-path must exist and have specific permissions. Should not be created with a docker volume
 mkdir -p "$KEYFILES_DIR"
 
-if grep -Fq "/opt/web3signer/keyfiles" ${KEYFILES_DIR}/*.yaml ;then
+if grep -Fq "/opt/web3signer/keyfiles" ${KEYFILES_DIR}/*.yaml; then
   sed -i "s|/opt/web3signer/keyfiles|$KEYFILES_DIR|g" ${KEYFILES_DIR}/*.yaml
 fi
 
